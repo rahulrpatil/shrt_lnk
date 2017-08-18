c4e436563bdb9a3853f148b63433dcc1eac6f8a2
Rahul Patil <rahulrptl@gmail.com>
2017-08-18 11:59:51 +0530

    complete basic first level shrt url app

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
+import LinkList from './LinksList';
+import { Meteor } from 'meteor/meteor';
 
 export default class Link extends React.Component {
     onLogout() {
       Accounts.logout(); 
+    }
+
+    onSubmit(e) {
+        const url = this.refs.url.value.trim();
+        e.preventDefault();
+
+        if(url) {
+            Meteor.call('links.insert', url);
+            this.refs.url.value = '';
+        }
     }
     render() {
         return (
             <div>
                 <h1>Your Links</h1>
                 <button onClick={this.onLogout.bind(this)}> Logout </button>
+                <LinkList/>
+                <p> Add Link</p>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="text" ref="url" placeholder="URL"/>
+                    <button>Add Link</button>
+                </form>    
             </div>
         )
   };
---
+++ imports/ui/LinksList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Tracker } from 'meteor/tracker';
+import { Links } from '../api/links';
+import { Meteor } from 'meteor/meteor';
+
+export default class LinksList extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            links: []
+        }
+    }
+    componentDidMount() {
+        console.log("mount");
+        this.linksTracker = Tracker.autorun(() => {
+            Meteor.subscribe('links');
+            const links = Links.find().fetch();
+            this.setState({ links })
+          }); 
+    }
+    componentWillUnmount() {
+        console.log("unmount");
+        this.linksTracker.stop();
+    }
+
+    renderLinksListItems() {
+        return (
+            this.state.links.map(link => {
+                return <p key={link._id}>{link.url}</p>
+            })
+        )
+    }
+    render() {
+        return (
+            <div>
+              <p> Links List </p> 
+              <div>
+                {this.renderLinksListItems()}
+              </div> 
+            </div>
+        );
+    }
+};
