06a827a4667814a622182c19fd23c326fc02ebd3
Rahul Patil <rahulrptl@gmail.com>
2017-08-31 09:22:30 +0530

    copy url to clipboard

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            justCopied: false
+        };
+    }
+
+    componentDidMount() {
+        this.clipboard = new Clipboard(this.refs.copy);
+        this.clipboard.on('success', () => {
+           this.setState({ justCopied: true });
+           setTimeout(() => this.setState({ justCopied: false }), 1000);
+        }).on('error', () => {
+            alert("Unable to copy. Please manually copy the link.");
+        })
+    }
+
+    componentWillUnmount() {
+        this.clipboard.destroy();
+    }
     
     render() {
         return (
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
+                <button ref="copy" data-clipboard-text={this.props.shortUrl}>
+                    { this.state.justCopied ? "Copied" : "Copy" }
+                </button>
             </div>
-        )
+        ) 
     }
 }
